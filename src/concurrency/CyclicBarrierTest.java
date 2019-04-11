7d5ed422faf9da68b8029baf7ab61f5ce77717ba
jim213 <991581463@qq.com>
2019-04-11 16:01:56 +0800

    con down

---
+++ src/concurrency/CyclicBarrierTest.java
@@ -0,0 +1,52 @@
+package concurrency;
+
+import java.util.concurrent.*;
+
+/**
+ *
+ * @author xuexiaolei
+ * @version 2017年11月02日
+ */
+public class CyclicBarrierTest {
+    private final static int THREAD_NUM = 10;
+
+    public static void main(String[] args) {
+        CyclicBarrier lock = new CyclicBarrier(THREAD_NUM, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("大家都准备完成了");
+            }
+        });
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < THREAD_NUM; i++) {
+            exec.submit(new CountdownLatchTask(lock, "Thread-" + i));
+        }
+        exec.shutdown();
+    }
+
+    static class CountdownLatchTask implements Runnable {
+        private final CyclicBarrier lock;
+        private final String threadName;
+
+        CountdownLatchTask(CyclicBarrier lock, String threadName) {
+            this.lock = lock;
+            this.threadName = threadName;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println(threadName + " 准备完成");
+                try {
+                    lock.await();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(threadName + " 执行完成");
+            }
+
+        }
+    }
+}
